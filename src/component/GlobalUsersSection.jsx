a4305d1150f4e405bb0b2ea8cfd71a791954ced1
Sahil Prasad <sahilprasad41969@gmailcom>
2025-12-07 20:49:26 +0530

    final commit

--- src/component/GlobalUsersSection.jsx
+++ src/component/GlobalUsersSection.jsx
@@ -4,7 +4,7 @@ const GlobalUsersSection = () => {
   return (
     <section className="py-16 px-4 sm:px-6 lg:px-8 bg-black">
       <div className="max-w-7xl mx-auto">
-        <p className="text-sm text-slate-400 font-thin mb-2 border-[0.8] rounded-2xl inline-block px-1 py-1 text-[#D6D2CD]">
+        <p className="text-sm font-thin mb-2 border-[0.5] rounded-2xl inline-block px-1 py-1 text-[#D6D2CD]">
           Our Solutions
         </p>
         <h2 className="text-4xl font-bold text-left mb-12">Global Users</h2>
